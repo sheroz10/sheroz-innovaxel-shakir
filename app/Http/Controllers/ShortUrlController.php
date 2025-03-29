39ab3c23974781129f42b9001db6535ebbdc62ba
Sheroz Shakir <sherozshakir786@gmail.com>
2025-03-29 23:13:34 +0500

    "Shorten and Retrieve Method is Created and their routes are set accordingly"

--- app/Http/Controllers/ShortUrlController.php
+++ app/Http/Controllers/ShortUrlController.php
@@ -2,9 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ShortUrl;
 use Illuminate\Http\Request;
 
 class ShortUrlController extends Controller
 {
-    //
+    public function shorten(Request $request)
+    {
+        $request->validate(['url' => 'required|url']);
+
+        $shortUrl = ShortUrl::create([
+            'original_url' => $request->url,
+        ]);
+
+        return response()->json([
+            'id' => $shortUrl->id,
+            'url' => $shortUrl->original_url,
+            'shortCode' => $shortUrl->short_code,
+            'createdAt' => $shortUrl->created_at,
+            'updatedAt' => $shortUrl->updated_at,
+        ], 201);
+    }
+
+public function retrieve($code)
+{
+    $shortUrl = ShortUrl::where('short_code', $code)->firstOrFail();
+
+    // Increment access count I have changed the name of the column to clicks
+    $shortUrl->increment('clicks');
+
+    return response()->json([
+        'id' => $shortUrl->id,
+        'url' => $shortUrl->original_url,
+        'shortCode' => $shortUrl->short_code,
+        'createdAt' => $shortUrl->created_at,
+        'updatedAt' => $shortUrl->updated_at,
+    ], 200);
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ShortUrlController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/shorten', [ShortUrlController::class, 'shorebten']);
+Route::get('/shorten/{code}', [ShortUrlController::class, 'retrieve']);
