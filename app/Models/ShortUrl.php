ae6ac98b5d546813e8bb6d44ca2dc4a8b1734a3e
Sheroz Shakir <sherozshakir786@gmail.com>
2025-03-29 23:09:33 +0500

    "Used Base 62 For Short URL creation"

--- app/Models/ShortUrl.php
+++ app/Models/ShortUrl.php
@@ -2,10 +2,20 @@
 
 namespace App\Models;
 
+use Tuupola\Base62;
 use Illuminate\Database\Eloquent\Model;
 
 class ShortUrl extends Model
 {
     protected $fillable = ['original_url', 'short_code', 'clicks'];
+
+    public static function boot()
+    {
+        parent::boot();
+        static::creating(function ($shortUrl) {
+            $base62 = new Base62();
+            $shortUrl->short_code = $base62->encode($shortUrl->id);
+        });
+    }
 }
 
